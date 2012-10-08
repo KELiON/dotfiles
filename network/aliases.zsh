bb887d3793219984173def6e8512cd07954537f9
Alexandr Subbotin <kelionweb@gmail.com>
2012-10-08 12:10:36 +0400

    aliases for ip addresses;

---
+++ network/aliases.zsh
@@ -0,0 +1,4 @@
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en1"
+alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
