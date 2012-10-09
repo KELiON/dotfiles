009a6d97bd1145aad1605572e7133ec3cef2cd07
Alexandr Subbotin <kelionweb@gmail.com>
2012-10-09 13:10:33 +0400

    change ruby aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,7 +1,2 @@
-alias r='rbenv local 1.8.7-p358'
-
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
-
-alias migrate='rake db:migrate db:test:clone'
+alias r='rbenv local 1.9.3-p0'
+alias migrate='rake db:migrate'
