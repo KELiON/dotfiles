e47e07c31ff0382dc0cd6e29b7213e2f00243159
Alexandr Subbotin <kelionweb@gmail.com>
2013-02-07 18:56:20 +0400

    bundle exec to rake migrate

--- ruby/aliases.fish
+++ ruby/aliases.fish
@@ -1,3 +1,3 @@
 alias r='rbenv local 1.9.3-p0'
-alias migrate='rake db:migrate'
-alias migrate:undo='rake db:rollback'
+alias migrate='bundle exec rake db:migrate'
+alias migrate:undo='bundle exec rake db:rollback'
