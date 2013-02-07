ac7ddb3010f1f2ac18272eaadb1573d4ca9c122f
Alexandr Subbotin <kelionweb@gmail.com>
2013-02-07 18:54:41 +0400

    Remove fish greetings

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,7 @@ set -g PROJECTS ~/projects
 set -g FISH $HOME/.config/fish
 set -g DOTFILES $HOME/.dotfiles
 
+set fish_greeting ""
+
 #prevent recursion
 for file in (ls $DOTFILES/**/*.fish | grep -v /fish/); . $file; end;
