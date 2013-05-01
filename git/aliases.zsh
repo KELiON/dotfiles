89d41d90ad1b28353616be59aeaf80f1e748ab67
Alexandr Subbotin <kelionweb@gmail.com>
2013-05-01 15:00:37 +0400

    gtracker git alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,4 +9,5 @@ fi
 # The rest of my fun git aliases
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gtracker="git log --pretty=format:'%Cred%h%Creset %C(yellow)%ad%Creset %s' --author='Alexandr Subbotin'"
 # more aliases in scm_breeze
