376a46957bfd880d1c952a172b08088ea9e78f70
Alexandr Subbotin <kelionweb@gmail.com>
2017-05-31 17:48:52 +0200

    Remove scm_breeze and add .g

---
+++ bin/.g
@@ -0,0 +1,19 @@
+if test "$1" == "show"
+then
+  git show $(.g log | head -c7)
+elif test "$1" == "log"
+then
+  git log --oneline | fzf --reverse
+elif test "$1" == "fixup"
+then
+  git commit --fixup=$(.g log | head -c7)
+elif test "$1" == "rebase"
+then
+  git rebase -i $(.g log | head -c7)
+elif test "$1" == "add"
+then
+  git add $(git status -s | fzf -m --reverse | cut -c 4-)
+elif test "$1" == "diff"
+then
+  git diff $(git ls-files -m | fzf)
+fi
--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,8 +6,18 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
+alias g="git"
+
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias gtracker="git log --pretty=format:'%Cred%h%Creset %C(yellow)%ad%Creset %s' --author='Alexandr Subbotin'"
-# more aliases in scm_breeze
+
+alias hb="hub browse"
+
+alias gpo="git push origin \$(gcb)"
+
+alias gs="git status"
+
+# .g shortcuts
+alias .ga=".g add"
+alias .gd=".g diff"
+alias .glog=".g log"
